d3eefb9b8a5459e789e964784755029a085b12b6
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-04-01 16:07:02 +0200

    Updates

---
+++ precache-manifest.e405bc78297e998d12cfda962492f9d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1653c6c48a68774b015cb2790b8081cb",
+    "url": "/Your-Games-Database/index.html"
+  },
+  {
+    "revision": "6fec49492c9304cf94b5",
+    "url": "/Your-Games-Database/static/css/2.d0121f86.chunk.css"
+  },
+  {
+    "revision": "3339a2bfd99168b035a7",
+    "url": "/Your-Games-Database/static/css/main.7342585f.chunk.css"
+  },
+  {
+    "revision": "6fec49492c9304cf94b5",
+    "url": "/Your-Games-Database/static/js/2.d34fac2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Your-Games-Database/static/js/2.d34fac2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3339a2bfd99168b035a7",
+    "url": "/Your-Games-Database/static/js/main.221e29d7.chunk.js"
+  },
+  {
+    "revision": "3ef3ca5b3c59db60bb45",
+    "url": "/Your-Games-Database/static/js/runtime-main.3e6e0f45.js"
+  },
+  {
+    "revision": "af3c090f6a81b6a526e3ce48e018a9c7",
+    "url": "/Your-Games-Database/static/media/background.af3c090f.png"
+  },
+  {
+    "revision": "dde008c38c316a6949d796cc6539083b",
+    "url": "/Your-Games-Database/static/media/retrowave.dde008c3.mp3"
+  },
+  {
+    "revision": "a338bc02ad9b3db647de771a8c4a0b48",
+    "url": "/Your-Games-Database/static/media/xmark.a338bc02.svg"
+  }
+]);
