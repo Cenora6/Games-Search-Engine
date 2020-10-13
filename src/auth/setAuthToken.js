e51710c00123c8e752b5ecf223953d95c7c3dd50
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-10-13 17:20:36 +0200

    Fixing bugs

--- src/auth/setAuthToken.js
+++
@@ -1,9 +0,0 @@
-import instance from './../axios/axios';
-
-export const setAuthToken = token => {
-    if (token) {
-        instance.defaults.headers.common['Authorization'] = token;
-    } else {
-        delete instance.defaults.headers.common['Authorization'];
-    }
-}
